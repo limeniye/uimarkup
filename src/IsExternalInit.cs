dc21ba5434a896e37969c67db546bd310c921c07
limeniye <limeniye@gmail.com>
2024-02-22 16:20:28 +0200

    chore: Remove IsExternalInit from Uno UI solution

--- src/IsExternalInit.cs
+++ src/IsExternalInit.cs
@@ -1,5 +1,7 @@
-﻿using System.ComponentModel;
+﻿#if NETSTANDARD2_0_OR_GREATER
+using System.ComponentModel;
 
 namespace System.Runtime.CompilerServices;
 [EditorBrowsable(EditorBrowsableState.Never)]
 public record IsExternalInit;
+#endif
