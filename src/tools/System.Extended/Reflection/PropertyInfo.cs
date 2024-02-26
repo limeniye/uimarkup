e18a68fad9cf8083e27bb71d4d808abd45e56e3b
limeniye <limeniye@gmail.com>
2024-02-26 18:46:40 +0200

    feat: PropertyInfo implementation

---
+++ src/tools/System.Extended/Reflection/PropertyInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace System.Extended.Reflection;
+
+/// <summary>
+/// Information about the type property
+/// </summary>
+/// <param name="SourceType">The type that contains this property</param>
+/// <param name="Property">Property type</param>
+/// <param name="Name">Property name</param>
+/// <remarks>
+/// Used in generated types
+/// </remarks>
+public sealed record PropertyInfo(Type SourceType, Type Property, string Name);
