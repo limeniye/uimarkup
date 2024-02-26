4d3969a08e4e84fcb4247142d2fd1599ac2d1cec
limeniye <limeniye@gmail.com>
2024-02-26 18:45:47 +0200

    feat: Color implementation

---
+++ src/implementations/UIMarkup.Protocol/Types/Color.cs
@@ -0,0 +1,20 @@
+﻿namespace UIMarkup.Protocol;
+
+public struct Color
+{
+	private string _hex;
+
+	public Color()
+	{
+		this = Transparent;
+	}
+
+	public Color(string hex)
+	{
+		_hex = hex;
+	}
+
+	public readonly static Color Transparent = new Color("#00FFFFFF");
+
+	public string Hex => _hex;
+}
