b0a314acf3607d84f10dc40851f9c8f050bac80f
limeniye <limeniye@gmail.com>
2024-03-01 14:05:37 +0200

    feat: Create Protocol generator project

---
+++ src/frameworks/UIMarkup.Protocol.SourceGenerators/Controls/ControlGenerator.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace UIMarkup.Protocol.SourceGenerators.Controls;
+
+[Generator]
+public class ControlGenerator : ISourceGenerator
+{
+	public void Execute(GeneratorExecutionContext context)
+	{
+
+	}
+
+	public void Initialize(GeneratorInitializationContext context)
+	{
+
+	}
+}
