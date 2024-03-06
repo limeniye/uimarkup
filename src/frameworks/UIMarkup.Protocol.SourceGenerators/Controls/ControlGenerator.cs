672f286186263faa9c6c46952a9abed26d5f773f
limeniye <limeniye@gmail.com>
2024-03-06 16:29:38 +0200

    chore: Implementation ControlGenerator with single control parse

--- src/frameworks/UIMarkup.Protocol.SourceGenerators/Controls/ControlGenerator.cs
+++ src/frameworks/UIMarkup.Protocol.SourceGenerators/Controls/ControlGenerator.cs
@@ -1,17 +1,84 @@
-﻿using Microsoft.CodeAnalysis;
+﻿using System.Text.Json;
+using System.Text;
+using Microsoft.CodeAnalysis;
 
-namespace UIMarkup.Protocol.SourceGenerators.Controls;
+namespace UIMarkup.Protocol.SourceGenerators;
 
 [Generator]
 public class ControlGenerator : ISourceGenerator
 {
 	public void Execute(GeneratorExecutionContext context)
 	{
+		// Get text for serialization
+		var schemaJson = context.AdditionalFiles.First();
+		var sourceText = schemaJson.GetText();
 
+		StringBuilder sbJson = new StringBuilder();
+		foreach (var line in sourceText.Lines)
+		{
+			sbJson.AppendLine(line.ToString());
+		}
+
+		// Json serialization
+		var schema = JsonSerializer.Deserialize<ControlsJsonSchema>(sbJson.ToString(), new JsonSerializerOptions
+		{
+			PropertyNameCaseInsensitive = true
+		});
+
+		if (schema.Controls.Count == 0)
+		{
+			return;
+		}
+
+		// Name and Code
+		List<(string TypeName, string TypeCode)> controlRecordsCode = new List<(string TypeName, string TypeCode)>(schema.Controls.Count);
+		foreach (var control in schema.Controls)
+		{
+			StringBuilder sbProperties = new StringBuilder();
+			var properties = control.Properties;
+			int i = 1;
+			foreach (var property in properties)
+			{
+				sbProperties.Append($"\t{property.Value} {property.Key}");
+				if (i != properties.Count)
+				{
+					sbProperties.AppendLine($",");
+					i++;
+				}
+			}
+			string controlRecordCode =
+$@"namespace UIMarkup.Protocol;
+
+public record {control.Type}(
+	Guid Guid{(sbProperties.Length > 0 ? ",\n" : string.Empty)}{sbProperties})
+{{
+}}
+";
+			controlRecordsCode.Add((control.Type, controlRecordCode));
+		}
+
+		foreach (var controlRecord in controlRecordsCode)
+		{
+			context.AddSource($"{controlRecord.TypeName}.g.cs", controlRecord.TypeCode);
+		}
 	}
 
-	public void Initialize(GeneratorInitializationContext context)
-	{
+	public void Initialize(GeneratorInitializationContext context) { }
+}
 
-	}
+public class ControlsJsonSchema
+{
+	public IReadOnlyCollection<ControlJsonSchemaType> Controls { get; init; } = Array.Empty<ControlJsonSchemaType>();
+}
+
+public class ControlJsonSchemaType
+{
+	public string Type { get; init; } = string.Empty;
+	public Dictionary<string, string> Properties { get; init; }
+}
+
+public class Property
+{
+	public string Name { get; init; } = string.Empty;
+	public string Type { get; init; } = string.Empty;
 }
