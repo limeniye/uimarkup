dd17aef279f09c4fc80a76dd260487a04c02d759
limeniye <limeniye@gmail.com>
2024-03-06 16:39:26 +0200

    chore(test): Generation test extensions

---
+++ src/frameworks/UIMarkup.Protocol.SourceGenerators.Tests/TestAdditionalFile.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.CodeAnalysis.Text;
+using Microsoft.CodeAnalysis;
+
+namespace UIMarkup.Protocol.SourceGenerators.Tests;
+
+public class TestAdditionalFile : AdditionalText
+{
+	private readonly string _text;
+	public override string Path { get; }
+
+	public TestAdditionalFile(string path, string text)
+	{
+		Path = path;
+		_text = text;
+	}
+
+	public override SourceText GetText(CancellationToken cancellationToken = new CancellationToken())
+	{
+		return SourceText.From(_text);
+	}
+}
---
+++ src/frameworks/UIMarkup.Protocol.SourceGenerators.Tests/TestsHelper.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using System.Collections.Immutable;
+
+namespace UIMarkup.Protocol.SourceGenerators.Tests;
+
+public static class TestsHelper
+{
+	public static ImmutableArray<AdditionalText> GetAdditionalText(string path)
+	{
+		var filePath = $"{Environment.CurrentDirectory}\\{path}";
+		var additional = new List<AdditionalText>();
+		if (path.Contains('*'))
+		{
+			throw new NotImplementedException();
+		}
+		else
+		{
+			var fileText = File.ReadAllText(filePath);
+			additional.Add(new TestAdditionalFile(filePath, fileText));
+		}
+		return additional.ToImmutableArray();
+	}
+
+	public static GeneratorDriver RunGeneratorDriver(
+		this ISourceGenerator generator,
+		ImmutableArray<AdditionalText>? additional = null,
+		IEnumerable<PortableExecutableReference> references = null)
+	{
+		var compilation = CSharpCompilation.Create(
+			assemblyName: "UIMarkup.Protocol.SourceGenerators",
+			references: references);
+
+		var driver = CSharpGeneratorDriver.Create(generator);
+		if (additional != null)
+		{
+			return driver
+				.AddAdditionalTexts((ImmutableArray<AdditionalText>)additional)
+				.RunGenerators(compilation);
+		}
+		return driver.RunGenerators(compilation);
+	}
+}
