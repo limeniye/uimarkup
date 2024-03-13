057584852e7ae9dd1da1eceffceb972445784941
limeniye <limeniye@gmail.com>
2024-03-13 18:22:04 +0200

    chore: Commit generated controls

--- src/frameworks/UIMarkup.Protocol/Controls/Control.cs
+++ src/frameworks/UIMarkup.Protocol/Controls/Control.cs
@@ -1,55 +1,55 @@
-﻿using System.Collections.ObjectModel;
-using System.Extended.Reflection;
+﻿//using System.Collections.ObjectModel;
+//using System.Extended.Reflection;
 
-namespace UIMarkup.Protocol;
+//namespace UIMarkup.Protocol;
 
-public record Control(
-	Guid Guid,
-	string Name = null,
-	double Width = 0, double Height = 0,
-	double MinWidth = 0, double MinHeight = 0,
-	double MaxWidth = 0, double MaxHeight = 0,
-	HorizontalAlignment HorizontalAlignment = HorizontalAlignment.Stretch,
-	VerticalAlignment VerticalAlignment = VerticalAlignment.Stretch,
-	Color Color = default,
-	Style Style = null,
-	Template Template = null,
-	Thickness Margin = default,
-	Thickness CornerRadius = default,
-	Thickness Stroke = default,
-	Color StrokeColor = default,
-	double Opacity = 1.0d,
-	bool IsVisible = true,
-	bool IsEnabled = true)
-		//
-		// Inheritance
-		//
-		: UIElement(Guid, Name, Width, Height, MinWidth, MinHeight,
-			MaxWidth, MaxHeight, HorizontalAlignment, VerticalAlignment,
-			Color, Style, Margin, Opacity, IsVisible, IsEnabled)
-{
-	/// <inheritdoc cref="UIElement.ProtocolPropertyValues" /> 
-	internal new ReadOnlyCollection<PropertyValue> ProtocolPropertyValues => new(
-	[
-		new PropertyValue(this, typeof(Guid), Guid),
-		new PropertyValue(this, typeof(string), Name),
-		new PropertyValue(this, typeof(double), Width),
-		new PropertyValue(this, typeof(double), Height),
-		new PropertyValue(this, typeof(double), MinWidth),
-		new PropertyValue(this, typeof(double), MinHeight),
-		new PropertyValue(this, typeof(double), MaxWidth),
-		new PropertyValue(this, typeof(double), MaxHeight),
-		new PropertyValue(this, typeof(HorizontalAlignment), HorizontalAlignment),
-		new PropertyValue(this, typeof(VerticalAlignment), VerticalAlignment),
-		new PropertyValue(this, typeof(Color), Color),
-		new PropertyValue(this, typeof(Style), Style),
-		new PropertyValue(this, typeof(Thickness), Margin),
-		new PropertyValue(this, typeof(double), Opacity),
-		new PropertyValue(this, typeof(bool), IsVisible),
-		new PropertyValue(this, typeof(bool), IsEnabled),
+//internal record Control(
+//	Guid Guid,
+//	string Name = null,
+//	double Width = 0, double Height = 0,
+//	double MinWidth = 0, double MinHeight = 0,
+//	double MaxWidth = 0, double MaxHeight = 0,
+//	HorizontalAlignment HorizontalAlignment = HorizontalAlignment.Stretch,
+//	VerticalAlignment VerticalAlignment = VerticalAlignment.Stretch,
+//	Color Color = default,
+//	Style Style = null,
+//	Template Template = null,
+//	Thickness Margin = default,
+//	Thickness CornerRadius = default,
+//	Thickness Stroke = default,
+//	Color StrokeColor = default,
+//	double Opacity = 1.0d,
+//	bool IsVisible = true,
+//	bool IsEnabled = true)
+//		//
+//		// Inheritance
+//		//
+//		: UIElement(Guid, Name, Width, Height, MinWidth, MinHeight,
+//			MaxWidth, MaxHeight, HorizontalAlignment, VerticalAlignment,
+//			Color, Style, Margin, Opacity, IsVisible, IsEnabled)
+//{
+//	/// <inheritdoc cref="UIElement.ProtocolPropertyValues" /> 
+//	internal new ReadOnlyCollection<PropertyValue> ProtocolPropertyValues => new(
+//	[
+//		new PropertyValue(this, typeof(Guid), Guid),
+//		new PropertyValue(this, typeof(string), Name),
+//		new PropertyValue(this, typeof(double), Width),
+//		new PropertyValue(this, typeof(double), Height),
+//		new PropertyValue(this, typeof(double), MinWidth),
+//		new PropertyValue(this, typeof(double), MinHeight),
+//		new PropertyValue(this, typeof(double), MaxWidth),
+//		new PropertyValue(this, typeof(double), MaxHeight),
+//		new PropertyValue(this, typeof(HorizontalAlignment), HorizontalAlignment),
+//		new PropertyValue(this, typeof(VerticalAlignment), VerticalAlignment),
+//		new PropertyValue(this, typeof(Color), Color),
+//		new PropertyValue(this, typeof(Style), Style),
+//		new PropertyValue(this, typeof(Thickness), Margin),
+//		new PropertyValue(this, typeof(double), Opacity),
+//		new PropertyValue(this, typeof(bool), IsVisible),
+//		new PropertyValue(this, typeof(bool), IsEnabled),
 
-		new PropertyValue(this, typeof(Thickness), CornerRadius),
-		new PropertyValue(this, typeof(Thickness), Stroke),
-		new PropertyValue(this, typeof(Color), StrokeColor)
-	]);
-}
+//		new PropertyValue(this, typeof(Thickness), CornerRadius),
+//		new PropertyValue(this, typeof(Thickness), Stroke),
+//		new PropertyValue(this, typeof(Color), StrokeColor)
+//	]);
+//}
--- src/frameworks/UIMarkup.Protocol/Controls/UIElement.cs
+++ src/frameworks/UIMarkup.Protocol/Controls/UIElement.cs
@@ -1,47 +1,47 @@
-﻿using System.Collections.ObjectModel;
-using System.Extended.Reflection;
+﻿//using System.Collections.ObjectModel;
+//using System.Extended.Reflection;
 
-namespace UIMarkup.Protocol;
+//namespace UIMarkup.Protocol;
 
-public record UIElement(
-	Guid Guid,
-	string Name = null,
-	double Width = 0, double Height = 0,
-	double MinWidth = 0, double MinHeight = 0,
-	double MaxWidth = 0, double MaxHeight = 0,
-	HorizontalAlignment HorizontalAlignment = HorizontalAlignment.Stretch,
-	VerticalAlignment VerticalAlignment = VerticalAlignment.Stretch,
-	Color Color = default,
-	Style Style = null,
-	Thickness Margin = default,
-	double Opacity = 1.0d,
-	bool IsVisible = true,
-	bool IsEnabled = true)
-{
-	/// <summary>
-	/// Contains properties and values ​​of generated UIElement types.
-	/// Replaced in derived classes.
-	/// </summary>
-	/// <remarks>
-	/// When generating a type that inherits from the UIElement class, the value of this field will be redefined via the new operator.
-	/// </remarks>
-	internal ReadOnlyCollection<PropertyValue> ProtocolPropertyValues => new(
-	[
-		new PropertyValue(this, typeof(Guid), Guid),
-		new PropertyValue(this, typeof(string), Name),
-		new PropertyValue(this, typeof(double), Width),
-		new PropertyValue(this, typeof(double), Height),
-		new PropertyValue(this, typeof(double), MinWidth),
-		new PropertyValue(this, typeof(double), MinHeight),
-		new PropertyValue(this, typeof(double), MaxWidth),
-		new PropertyValue(this, typeof(double), MaxHeight),
-		new PropertyValue(this, typeof(HorizontalAlignment), HorizontalAlignment),
-		new PropertyValue(this, typeof(VerticalAlignment), VerticalAlignment),
-		new PropertyValue(this, typeof(Color), Color),
-		new PropertyValue(this, typeof(Style), Style),
-		new PropertyValue(this, typeof(Thickness), Margin),
-		new PropertyValue(this, typeof(double), Opacity),
-		new PropertyValue(this, typeof(bool), IsVisible),
-		new PropertyValue(this, typeof(bool), IsEnabled)
-	]);
-}
+//internal record UIElement(
+//	Guid Guid,
+//	string Name = null,
+//	double Width = 0, double Height = 0,
+//	double MinWidth = 0, double MinHeight = 0,
+//	double MaxWidth = 0, double MaxHeight = 0,
+//	HorizontalAlignment HorizontalAlignment = HorizontalAlignment.Stretch,
+//	VerticalAlignment VerticalAlignment = VerticalAlignment.Stretch,
+//	Color Color = default,
+//	Style Style = null,
+//	Thickness Margin = default,
+//	double Opacity = 1.0d,
+//	bool IsVisible = true,
+//	bool IsEnabled = true)
+//{
+//	/// <summary>
+//	/// Contains properties and values ​​of generated UIElement types.
+//	/// Replaced in derived classes.
+//	/// </summary>
+//	/// <remarks>
+//	/// When generating a type that inherits from the UIElement class, the value of this field will be redefined via the new operator.
+//	/// </remarks>
+//	internal ReadOnlyCollection<PropertyValue> ProtocolPropertyValues => new(
+//	[
+//		new PropertyValue(this, typeof(Guid), Guid),
+//		new PropertyValue(this, typeof(string), Name),
+//		new PropertyValue(this, typeof(double), Width),
+//		new PropertyValue(this, typeof(double), Height),
+//		new PropertyValue(this, typeof(double), MinWidth),
+//		new PropertyValue(this, typeof(double), MinHeight),
+//		new PropertyValue(this, typeof(double), MaxWidth),
+//		new PropertyValue(this, typeof(double), MaxHeight),
+//		new PropertyValue(this, typeof(HorizontalAlignment), HorizontalAlignment),
+//		new PropertyValue(this, typeof(VerticalAlignment), VerticalAlignment),
+//		new PropertyValue(this, typeof(Color), Color),
+//		new PropertyValue(this, typeof(Style), Style),
+//		new PropertyValue(this, typeof(Thickness), Margin),
+//		new PropertyValue(this, typeof(double), Opacity),
+//		new PropertyValue(this, typeof(bool), IsVisible),
+//		new PropertyValue(this, typeof(bool), IsEnabled)
+//	]);
+//}
