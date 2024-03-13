e0c9efecf4d6c7d59bbd9dec3b65057998b3a386
limeniye <limeniye@gmail.com>
2024-03-13 18:21:49 +0200

    chore: Hot changes

---
+++ src/frameworks/UIMarkup.Protocol.Standard/Class1.cs
@@ -0,0 +1,8 @@
+﻿namespace UIMarkup.Protocol.Standard;
+internal class Class1
+{
+#pragma warning disable IDE0051 // Remove unused private members
+	//private readonly ContentControl _test = null;
+
+#pragma warning restore IDE0051 // Remove unused private members
+}
