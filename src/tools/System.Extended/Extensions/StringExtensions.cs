458ec9ed52c0992fcb8b13d2b3e06526bbe13dac
limeniye <limeniye@gmail.com>
2024-03-15 15:13:16 +0200

    chore: Add StringExtensions JoinBy

---
+++ src/tools/System.Extended/Extensions/StringExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace System.Extended;
+internal static class StringExtensions
+{
+	public static string JoinBy(this IEnumerable<string> items, string joinBy)
+	{
+		return string.Join(joinBy, items);
+	}
+}
